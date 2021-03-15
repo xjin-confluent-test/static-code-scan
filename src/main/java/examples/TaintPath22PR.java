a684867b17c62df8dffa694b22158715b82d3318
Xin Jin <xjin@confluent.io>
2021-03-15 12:43:04 -0700

    Minor changes

--- src/main/java/examples/TaintPath22PR.java
+++ src/main/java/examples/TaintPath22PR.java
@@ -18,9 +18,10 @@ public class TaintPath22PR {
                 new InputStreamReader(sock.getInputStream(), "UTF-8"));
         String filename = filenameReader.readLine();
         String sanitizedPath = Util.get(filename);
+        String sanitizedPath2 = Util.get(filename);
         // BAD: read from a file using a path controlled by the user
         BufferedReader fileReader = new BufferedReader(
-                new FileReader("/home/" + user + "/" + sanitizedPath));
+                new FileReader("/home/" + user + "/" + sanitizedPath2));
         String fileLine = fileReader.readLine();
         while(fileLine != null) {
             sock.getOutputStream().write(fileLine.getBytes());
